c197e125fdf3b339d33010029f16cacf04e07ff8
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2023-10-17 16:25:53 -0300

    feat(loading-indicator): add new component

---
+++ src/components/LoadingIndicator/LoadingIndicator.tsx
@@ -0,0 +1,9 @@
+import { Container, ActivityIndicator } from './styles';
+
+export default function LoadingIndicator() {
+  return (
+    <Container>
+      <ActivityIndicator />
+    </Container>
+  );
+}
---
+++ src/components/LoadingIndicator/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.gray_600};
+`;
+
+export const ActivityIndicator = styled.ActivityIndicator.attrs(
+  ({ theme }) => ({
+    color: theme.colors.green_700,
+  }),
+)``;
