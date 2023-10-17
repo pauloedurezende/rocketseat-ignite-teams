19d98b4554c5707ecbf4a98fbc323e20fc078916
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2023-10-17 16:26:32 -0300

    feat(app): use roboto 400 and 700 font weigth

--- App.tsx
+++ App.tsx
@@ -1,15 +1,26 @@
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
 import { Routes } from '@routes/index';
+import { LoadingIndicator } from '@components/LoadingIndicator';
 
 import theme from './src/theme';
 
 export default function App() {
+  const [fontsHaveBeenLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar />
-      <Routes />
+      {fontsHaveBeenLoaded ? <Routes /> : <LoadingIndicator />}
     </ThemeProvider>
   );
 }
