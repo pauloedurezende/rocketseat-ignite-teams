c08c79048c3f669670be759c9211c937ed4ac20f
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2023-10-17 17:53:10 -0300

    feat(teams): use highlight component on screen

--- src/screens/Teams/Teams.tsx
+++ src/screens/Teams/Teams.tsx
@@ -1,11 +1,14 @@
 import { Container } from './styles';
 
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 
 export default function Teams() {
   return (
     <Container>
       <Header />
+
+      <Highlight title="Teams" subtitle="play with your team" />
     </Container>
   );
 }
