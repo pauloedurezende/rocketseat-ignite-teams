a1baf9c55d9621941db62dede54568107614751d
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2023-10-17 15:51:11 -0300

    refactor(routes): improve architecture

--- App.tsx
+++ App.tsx
@@ -1,23 +1,15 @@
 import { StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ThemeProvider } from 'styled-components/native';
 
-import { Home } from '@screens/Home';
+import { Routes } from '@routes/index';
 
 import theme from './src/theme';
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar />
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Routes />
     </ThemeProvider>
   );
 }
