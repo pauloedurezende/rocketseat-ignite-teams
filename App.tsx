3e9337c9a1a742c384fec8541b18ba8d61a86bbc
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2023-10-17 15:39:41 -0300

    feat(styled-components): add custom theme

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ThemeProvider } from 'styled-components/native';
 
 import { Home } from '@screens/Home';
+
+import theme from './src/theme';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar />
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
       </NavigationContainer>
-    </>
+    </ThemeProvider>
   );
 }
