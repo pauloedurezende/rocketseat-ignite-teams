3996d0b588427c1b287242cf8b0a88c2e4942e1c
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2023-10-17 17:52:46 -0300

    feat(highlight): add new component

---
+++ src/components/Highlight/Highlight.tsx
@@ -0,0 +1,15 @@
+import { Container, Title, SubTitle } from './styles';
+
+type HighlightProps = {
+  title: string;
+  subtitle: string;
+};
+
+export default function Highlight({ title, subtitle }: HighlightProps) {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <SubTitle>{subtitle}</SubTitle>
+    </Container>
+  );
+}
