cd277791f6bcaf3ec33439cb251f6159d5f87d0a
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2023-10-18 10:17:33 -0300

    feat(teams): add ability to list registered teams

--- src/screens/Teams/Teams.tsx
+++ src/screens/Teams/Teams.tsx
@@ -1,14 +1,32 @@
-import { Container } from './styles';
+import { useState } from 'react';
+import { FlatList, Text } from 'react-native';
 
+import { Team } from '@types';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { ListEmptyItem } from '@components/ListEmptyItem';
+
+import { Container } from './styles';
 
 export default function Teams() {
+  const [teams, setTeams] = useState<Team[]>([]);
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Teams" subtitle="play with your team" />
+
+      <FlatList
+        data={teams}
+        keyExtractor={(data) => data.id}
+        contentContainerStyle={teams.length === 0 && { flex: 1 }}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => <Text key={item.id}>{item.name}</Text>}
+        ListEmptyComponent={() => (
+          <ListEmptyItem message="How about registering the first team?" />
+        )}
+      />
     </Container>
   );
 }
