3f5469c1848737e47481c356f64e5a3a8a1439a6
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2023-10-20 15:07:29 -0300

    chore(paths): add utils

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,7 @@ module.exports = function (api) {
             '@assets': './assets',
             '@types': './src/@types',
             '@storage': './src/storage',
+            '@utils': './src/utils',
           },
         },
       ],
