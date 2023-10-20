a03d8fcba4bd34f5c4caa91bcf53565458816a1e
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2023-10-20 14:19:52 -0300

    chore(paths): add storage

--- babel.config.js
+++ babel.config.js
@@ -13,6 +13,7 @@ module.exports = function (api) {
             '@components': './src/components',
             '@assets': './assets',
             '@types': './src/@types',
+            '@storage': './src/storage',
           },
         },
       ],
