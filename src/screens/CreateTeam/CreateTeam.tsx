346ba8c540e2932b9c776240ee4a5024068bda75
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2023-10-20 15:29:48 -0300

    feat(create-team): add hability to register new team

--- src/screens/CreateTeam/CreateTeam.tsx
+++ src/screens/CreateTeam/CreateTeam.tsx
@@ -1,14 +1,43 @@
 import { useState } from 'react';
+import { Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { Highlight } from '@components/Highlight';
 import { Header } from '@components/Header';
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 
+import { teams } from '@storage/teams';
+
 import { Container, Content, Icon } from './styles';
+import { ApplicationError } from '@utils/application-error';
 
 export default function CreateTeam() {
   const [teamName, setTeamName] = useState('');
+
+  const { navigate } = useNavigation();
+
+  async function handleOnButtonPress() {
+    try {
+      const trimmedTeamName = teamName.trim();
+      const hasTeamName = trimmedTeamName.length > 0;
+
+      if (!hasTeamName) {
+        return Alert.alert('Oops!', 'Please enter a valid team name');
+      }
+
+      await teams.create(trimmedTeamName);
+
+      navigate('teams');
+    } catch (error) {
+      if (error instanceof ApplicationError) {
+        return Alert.alert('Oops!', error?.message);
+      }
+
+      Alert.alert('Oops!', 'Something went wrong, try again later');
+      console.error(error);
+    }
+  }
 
   return (
     <Container>
@@ -27,7 +56,9 @@ export default function CreateTeam() {
           onChangeText={(text) => setTeamName(text)}
         />
 
-        <Button style={{ marginTop: 20 }}>Create</Button>
+        <Button style={{ marginTop: 20 }} onPress={handleOnButtonPress}>
+          Create
+        </Button>
       </Content>
     </Container>
   );
