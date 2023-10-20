d007f410c7af74827e060bc6844e1a4727c868bd
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2023-10-20 13:59:25 -0300

    feat(create-team): add components to the screen

--- src/screens/CreateTeam/CreateTeam.tsx
+++ src/screens/CreateTeam/CreateTeam.tsx
@@ -1,3 +1,27 @@
+import { Highlight } from '@components/Highlight';
+import { Header } from '@components/Header';
+import { Button } from '@components/Button';
+import { Input } from '@components/Input';
+
+import { Container, Content, Icon } from './styles';
+
 export default function CreateTeam() {
-  return null;
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Content>
+        <Icon />
+
+        <Highlight
+          title="New Team"
+          subtitle="Create a new team to add people"
+        />
+
+        <Input placeholder="Team Name" onChangeText={() => {}} />
+
+        <Button style={{ marginTop: 20 }}>Create</Button>
+      </Content>
+    </Container>
+  );
 }
--- src/screens/CreateTeam/styles.ts
+++ src/screens/CreateTeam/styles.ts
@@ -1,4 +1,25 @@
 import styled from 'styled-components/native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
-export const Container = styled(SafeAreaView)``;
+import { KeyboardAvoidingView, Platform } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { UsersThree } from 'phosphor-react-native';
+
+export const Container = styled(SafeAreaView)`
+  background-color: ${({ theme }) => theme.colors.gray_600};
+  flex: 1;
+  padding: 24px;
+`;
+
+export const Content = styled(KeyboardAvoidingView).attrs({
+  behavior: Platform.OS === 'ios' ? 'padding' : 'height',
+})`
+  flex: 1;
+  justify-content: center;
+`;
+
+export const Icon = styled(UsersThree).attrs(({ theme }) => ({
+  color: theme.colors.green_700,
+  size: 56,
+}))`
+  align-self: center;
+`;
