a29437e357bc523b33bb580857c3e009aff90c7f
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2023-10-24 16:40:27 -0300

    feat(players): receive team data as route params

--- src/screens/Players/Players.tsx
+++ src/screens/Players/Players.tsx
@@ -1,11 +1,21 @@
 import { Text } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import type { RouteProp } from '@react-navigation/native';
+
+import { RootStackParamList } from '@types';
 
 import { Container } from './styles';
 
+type PlayersScreenRouteProps = RouteProp<RootStackParamList, 'players'>;
+
 export default function Players() {
+  const {
+    params: { team },
+  } = useRoute<PlayersScreenRouteProps>();
+
   return (
     <Container>
-      <Text>Players - Screen</Text>
+      <Text>Players - Screen - {team?.name}</Text>
     </Container>
   );
 }
