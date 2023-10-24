9035928c76c71b6b9edad01a195f6272168fa34d
Paulo Eduardo Rezende <pauloeduardorezende@outlook.com>
2023-10-24 17:12:53 -0300

    feat(players): add component to show available sides

--- src/screens/Players/Players.tsx
+++ src/screens/Players/Players.tsx
@@ -1,19 +1,24 @@
-import { Text } from 'react-native';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import type { RouteProp } from '@react-navigation/native';
 
-import { RootStackParamList } from '@types';
+import { Player, RootStackParamList, TeamSide } from '@types';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
 import { ButtonIcon } from '@components/ButtonIcon';
+import { Filter } from '@components/Filter';
 
-import { Container, Form } from './styles';
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 
 type PlayersScreenRouteProps = RouteProp<RootStackParamList, 'players'>;
 
 export default function Players() {
+  const [players, setPlayers] = useState<Player[]>([]);
+  const [selectedTeamSide, setSelectedTeamSide] = useState(TeamSide.Home);
+
   const {
     params: { team },
   } = useRoute<PlayersScreenRouteProps>();
@@ -35,6 +40,23 @@ export default function Players() {
         />
         <ButtonIcon icon="add" />
       </Form>
+
+      <HeaderList>
+        <FlatList
+          horizontal
+          data={[TeamSide.Home, TeamSide.Away]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={selectedTeamSide === item}
+              onPress={() => setSelectedTeamSide(item)}
+            />
+          )}
+        />
+
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
