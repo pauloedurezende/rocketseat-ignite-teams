a1ab649865dc0b94c13eb29de8087f6838d6bb45
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2023-10-24 22:35:45 -0300

    feat(players): add ability to list players based on side

--- src/screens/Players/Players.tsx
+++ src/screens/Players/Players.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Alert, FlatList, TextInput } from 'react-native';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import type { RouteProp } from '@react-navigation/native';
@@ -6,6 +6,7 @@ import type { RouteProp } from '@react-navigation/native';
 import { Player, RootStackParamList, TeamSide } from '@types';
 import { teams } from '@storage/teams';
 import { player } from '@storage/player';
+import { ApplicationError } from '@utils/application-error';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -15,15 +16,16 @@ import { Filter } from '@components/Filter';
 import { PlayerCard } from '@components/PlayerCard';
 import { ListEmptyItem } from '@components/ListEmptyItem';
 import { Button } from '@components/Button';
+import { LoadingIndicator } from '@components/LoadingIndicator';
 
 import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
-import { ApplicationError } from '@utils/application-error';
 
 type PlayersScreenRouteProps = RouteProp<RootStackParamList, 'players'>;
 
 export default function Players() {
   const playerNameInputRef = useRef<TextInput>(null);
 
+  const [isLoading, setIsLoading] = useState(false);
   const [playerName, setPlayerName] = useState('');
   const [players, setPlayers] = useState<Player[]>([]);
   const [selectedTeamSide, setSelectedTeamSide] = useState(TeamSide.Home);
@@ -32,6 +34,24 @@ export default function Players() {
     params: { team },
   } = useRoute<PlayersScreenRouteProps>();
   const { navigate } = useNavigation();
+
+  async function fetchAllPlayersBasedOnSide() {
+    try {
+      setIsLoading(true);
+
+      const players = await player.getPlayersBySideAndTeam(
+        selectedTeamSide,
+        team?.id,
+      );
+
+      setPlayers(players);
+    } catch (error) {
+      Alert.alert('Error', 'An error occurred while fetching the players');
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   async function handleNewPlayer() {
     const trimmedPlayerName = playerName.trim();
@@ -52,6 +72,7 @@ export default function Players() {
       playerNameInputRef.current?.blur();
 
       setPlayerName('');
+      fetchAllPlayersBasedOnSide();
     } catch (error) {
       if (error instanceof ApplicationError) {
         return Alert.alert('Oops!', error.message);
@@ -80,6 +101,10 @@ export default function Players() {
     ]);
   }
 
+  useEffect(() => {
+    fetchAllPlayersBasedOnSide();
+  }, [selectedTeamSide]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -99,7 +124,7 @@ export default function Players() {
           returnKeyType="done"
           value={playerName}
         />
-        <ButtonIcon icon="add" />
+        <ButtonIcon icon="add" onPress={handleNewPlayer} />
       </Form>
 
       <HeaderList>
@@ -119,21 +144,25 @@ export default function Players() {
         <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderList>
 
-      <FlatList
-        data={players}
-        keyExtractor={(item) => item.id}
-        showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => (
-          <PlayerCard name={item.name} onRemove={() => {}} />
-        )}
-        ListEmptyComponent={() => (
-          <ListEmptyItem message="This team has no registered players" />
-        )}
-        contentContainerStyle={[
-          { paddingBottom: 100 },
-          players.length === 0 && { flex: 1 },
-        ]}
-      />
+      {isLoading ? (
+        <LoadingIndicator />
+      ) : (
+        <FlatList
+          data={players}
+          keyExtractor={(item) => item.id}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <PlayerCard name={item.name} onRemove={() => {}} />
+          )}
+          ListEmptyComponent={() => (
+            <ListEmptyItem message="This team has no registered players" />
+          )}
+          contentContainerStyle={[
+            { paddingBottom: 100 },
+            players.length === 0 && { flex: 1 },
+          ]}
+        />
+      )}
 
       <Button type="secondary" onPress={handleRemoveTeam}>
         Remove Team
--- src/storage/player/index.ts
+++ src/storage/player/index.ts
@@ -1,7 +1,9 @@
 import { create } from './create';
 import { getPlayersByTeam } from './get-players-by-team';
+import { getPlayersBySideAndTeam } from './get-players-by-side-and-team';
 
 export const player = {
   create,
   getPlayersByTeam,
+  getPlayersBySideAndTeam,
 };
